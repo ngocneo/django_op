75e08cb541f044082cc08cada56829e1346e5c9f
nmngoc1 <nmngoc1@cmcglobal.vn>
2022-08-25 17:22:48 +0700

    update

--- members/views.py
+++ members/views.py
@@ -2,6 +2,41 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from .models import Members
+from .serializers import MemberSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+
+class MembersApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 1. List all
+    def get(self, request, *args, **kwargs):
+        '''
+        List all the todo items for given requested user
+        '''
+        todos = Members.objects.filter(user = request.user.id)
+        serializer = MemberSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # 2. Create
+    def post(self, request, *args, **kwargs):
+        '''
+        Create the Todo with given todo data
+        '''
+        data = {
+            'task': request.data.get('firstname'), 
+            'completed': request.data.get('lastname'), 
+            'user': request.user.id
+        }
+        serializer = MemberSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def index(request):
   mymembers = Members.objects.all().values()
