8918d9f19a44103be2fff9c10fb180cd23dac418
nmngoc1 <nmngoc1@cmcglobal.vn>
2022-08-25 16:58:05 +0700

    update

--- myworld/settings.py
+++ myworld/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'members.apps.MembersConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
